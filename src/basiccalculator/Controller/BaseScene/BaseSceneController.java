dd1af61ba23a0359eaaa88fa6d55ac01ffe6df70
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-09 15:12:21 +0600

    Calculator UI completed

---
+++ src/basiccalculator/Controller/BaseScene/BaseSceneController.java
@@ -0,0 +1,25 @@
+
+package basiccalculator.Controller.BaseScene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class BaseSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private void handleClick(ActionEvent Event){
+        System.out.println("Hello worldsdff");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
